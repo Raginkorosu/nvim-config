7cd84ff0635ba072e377b9b5d7152e4d259a19a3
= <robbywalters107@gmail.com>
2023-01-13 17:13:15 -0600

    Added some diagnostice keymaps and added startuptime plugin to profile

--- lua/core/lsp/handlers.lua
+++ lua/core/lsp/handlers.lua
@@ -17,7 +17,7 @@ M.setup = function()
         virtual_text = true,
         -- show signs
         signs = {
-          active = signs,
+            active = signs,
         },
 
         update_in_insert = true,
@@ -25,26 +25,29 @@ M.setup = function()
         severity_sort = true,
     }
 
-  vim.diagnostic.config(config)
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set('n', '<space>d', vim.diagnostic.open_float, opts);
+    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts);
+    vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts);
+    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts);
+    vim.diagnostic.config(config)
 end
 
 M.on_attach = function(client, bufnr)
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-    local bufopts  = { noremap = true, silent = true, buffer=bufnr }
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
     vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
     vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    vim.keymap.set('n', '<space>wl', function()
-    end, bufopts)
     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
     vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "<S><Mod><f>", vim.lsp.buf.formatting, bufopts)
+    vim.keymap.set("n", "<M-S-f>", function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 
 
@@ -52,8 +55,8 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
-print("cmp_nvim_lsp plugin did not load in properly")
-return
+    vim.notify("cmp_nvim_lsp plugin did not load in properly")
+    return
 end
 
 M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
